4a8fe7f2739b3edaef8d96adac97d7ef3b5804eb
KAIbsb <lbky_1123@163.com>
2026-04-26 03:05:09 +0800

    UI管理器及面板基类更新 同时更新了gitignore文件 加载了TMP文件（文字UI）

---
+++ TheLaw/Assets/Scripts/Core/Managers/UIManager.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public enum E_UILayer
+{
+    Loading,
+    Top,
+    Middle,
+    Bottom
+}
+
+public class UIManager : ManagerBase<UIManager>
+{
+
+    private Camera uiCamera;
+    private Canvas uiCanvas;
+    private EventSystem uiEventSystem;
+    #region 层级
+    private Transform loadingLayer;
+    private Transform topLayer;
+    private Transform middleLayer;
+    private Transform bottomLayer;
+    #endregion
+    /// <summary>
+    /// // 初始化必要对象及组件
+    /// </summary>
+    private UIManager()
+    {
+        // 初始化对象 们
+        uiCamera = Object.Instantiate(Resources.Load<Camera>("Prefabs/UI/UI Camera")).GetComponent<Camera>();
+        uiCanvas = Object.Instantiate(Resources.Load<Canvas>("Prefabs/UI/Canvas")).GetComponent<Canvas>();
+        uiCanvas.worldCamera = uiCamera;
+        uiEventSystem = Object.Instantiate(Resources.Load<EventSystem>("Prefabs/UI/EventSystem")).GetComponent<EventSystem>();
+        // 添加层级
+        loadingLayer = uiCanvas.transform.Find("Loading");
+        topLayer = uiCanvas.transform.Find("Top");
+        middleLayer = uiCanvas.transform.Find("Middle");
+        bottomLayer = uiCanvas.transform.Find("Bottom");
+    }
+    // 存面板的字典
+    public Dictionary<string, PanelBase> panels = new Dictionary<string, PanelBase>();
+
+    /// <summary>
+    /// 向字典加入面板
+    /// </summary>
+    /// <param name="panel">面板对象</param>
+    /// <typeparam name="T">面板类型</typeparam>
+    public void AddPanel<T>(PanelBase panel) where T : PanelBase
+    {
+        panels.Add(typeof(T).Name, panel);
+    }
+
+    /// <summary>
+    /// 创建指定类型的面板、将其加入字典、设置父对象（移动到对应层级）、最后显示面板
+    /// </summary>
+    /// <param name="layer"></param>
+    /// <typeparam name="T"></typeparam>
+    public void CreatPanel<T>(E_UILayer layer = E_UILayer.Bottom) where T : PanelBase
+    {
+        GameObject panel = Object.Instantiate(Resources.Load<GameObject>($"Prefabs/UI/Panels/{typeof(T).Name}"), GetUILayer(layer), false);
+        panel.AddComponent<T>();
+        T panelComponent = panel.GetComponent<T>();
+        panels.Add(typeof(T).Name, panelComponent);
+        panels[typeof(T).Name].ShowSelf();
+    }
+
+    /// <summary>
+    /// 隐藏指定类型的面板，然后将其删除并从字典中移除
+    /// </summary>
+    /// <typeparam name="T">面板类型</typeparam>
+    public void RemovePanel<T>() where T : PanelBase
+    {
+        string name = typeof(T).Name;
+        T panel = panels[name] as T;
+        panel.HideSelf(() => { Object.Destroy(panel.gameObject); });
+        panels.Remove(name);
+    }
+    /// <summary>
+    /// 获取面板层级对象的transform组件
+    /// </summary>
+    /// <param name="layer">层级枚举</param>
+    /// <returns>对应层级的transform</returns>
+    public Transform GetUILayer(E_UILayer layer)
+    {
+        switch (layer)
+        {
+            case E_UILayer.Loading:
+                return loadingLayer;
+            case E_UILayer.Top:
+                return topLayer;
+            case E_UILayer.Middle:
+                return middleLayer;
+            case E_UILayer.Bottom:
+                return bottomLayer;
+            default:
+                return null;
+        }
+    }
+    /// <summary>
+    /// 返回对应类型的面板
+    /// </summary>
+    /// <typeparam name="T">面板类型</typeparam>
+    /// <returns>有类型返回对应面板，没有返回空</returns>
+    public PanelBase GetPanel<T>() where T : PanelBase
+    {
+        if (panels.ContainsKey(typeof(T).Name))
+        {
+            return panels[typeof(T).Name] as T;
+        }
+        return null;
+    }
+}
---
+++ TheLaw/Assets/Scripts/Gameplay/UI/PanelBase.cs
@@ -0,0 +1,190 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+// 面板基类
+public abstract class PanelBase : MonoBehaviour
+{
+
+    protected virtual void Awake()
+    {
+        // 淡入淡出，创建canvasgroup组件
+        canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+        {
+            canvasGroup = gameObject.AddComponent<CanvasGroup>();
+        }
+        // 初始化面板字典，把组件塞进去
+        // 组合组件先进入
+        SetControlsByType<Button>();
+        SetControlsByType<Toggle>();
+        SetControlsByType<Slider>();
+        SetControlsByType<InputField>();
+        SetControlsByType<ScrollRect>();
+        SetControlsByType<Dropdown>();
+        // 普通组件后进入
+        SetControlsByType<Text>();
+        SetControlsByType<TextMeshProUGUI>();
+        SetControlsByType<Image>();
+    }
+
+    protected virtual void Update()
+    {
+        // 淡入淡出
+        if (isShow && canvasGroup.alpha != 1)
+        {
+            canvasGroup.alpha += alphaSpeed * Time.deltaTime;
+            if (canvasGroup.alpha >= 1)
+            {
+                canvasGroup.alpha = 1;
+            }
+        }
+        else if (!isShow && canvasGroup.alpha != 0)
+        {
+            canvasGroup.alpha -= alphaSpeed * Time.deltaTime;
+            if (canvasGroup.alpha <= 0)
+            {
+                canvasGroup.alpha = 0;
+                // 完全隐藏后执行销毁委托
+                hideCallBack?.Invoke();
+            }
+        }
+    }
+
+    #region 存储子对象组件并添加监听
+    // 存储面板上所有组件的字典，按组件种类分类
+    public Dictionary<string, UIBehaviour> controls = new Dictionary<string, UIBehaviour>();
+
+    // 组件默认名，用来筛选默认名组件，默认名组件不会进入字典
+    private static List<string> defaultNames = new List<string>()
+    {
+        "Image",
+        "RawImage",
+        "Text (TMP)",
+        "Text (Legacy)",
+        "Label",
+        "Background",
+        "Checkmark",
+        "Arrow",
+        "Placeholder",
+        "Fill",
+        "Handle",
+        "Viewport",
+        "Scrollbar Horizontal",
+        "Scrollbar Vertical",
+    };
+
+    /// <summary>
+    /// 寻找子对象组件，塞入字典并按类型设置对应监听
+    /// </summary>
+    /// <typeparam name="T">需要加入字典的组件种类</typeparam>
+    private void SetControlsByType<T>() where T : UIBehaviour
+    {
+        // 得到所有子对象组件
+        T[] childrenControls = GetComponentsInChildren<T>(true);
+        // 遍历填充
+        for (int i = 0; i < childrenControls.Length; i++)
+        {
+            string currentName = childrenControls[i].gameObject.name;
+            // 筛选默认名
+            if (!defaultNames.Contains(currentName))
+            {
+                // 如果字典没有同名组件，才进入字典，防报错
+                try
+                {
+                    controls.Add(currentName, childrenControls[i]);
+                }
+                catch (Exception e)
+                {
+                    print(e);
+                }
+            }
+            // 设置监听
+            if (childrenControls[i] is Button)
+            {
+                (childrenControls[i] as Button).onClick.AddListener(() =>
+                {
+                    ButtonOnClick(currentName);
+                });
+            }
+            else if (childrenControls[i] is Slider)
+            {
+                (childrenControls[i] as Slider).onValueChanged.AddListener((value) =>
+                {
+                    SliderOnValueChanged(currentName, value);
+                });
+            }
+            else if (childrenControls[i] is Toggle)
+            {
+                (childrenControls[i] as Toggle).onValueChanged.AddListener((value) =>
+                {
+                    ToggleOnValueChanged(currentName, value);
+                });
+            }
+        }
+    }
+    // 不同组件添加监听的虚方法
+    protected virtual void ButtonOnClick(string buttonName) { }
+    protected virtual void SliderOnValueChanged(string sliderName, float value) { }
+    protected virtual void ToggleOnValueChanged(string toggleName, bool value) { }
+    #endregion
+    #region 面板显隐时执行的方法
+    // 淡入淡出
+    public bool isShow = false;
+    private CanvasGroup canvasGroup;
+    private int alphaSpeed = 5;
+    // 隐藏后让管理器销毁自己
+    private UnityAction hideCallBack;
+    /// <summary>
+    /// 淡入
+    /// </summary>
+    public virtual void ShowSelf()
+    {
+        isShow = true;
+        canvasGroup.alpha = 0;
+    }
+    /// <summary>
+    /// 淡出
+    /// </summary>
+    /// <param name="callBack">淡出完成后要执行的委托</param>
+    public virtual void HideSelf(UnityAction callBack)
+    {
+        isShow = false;
+        // 没有淡入一半的情况关掉的（根本没协程），所以还是设置一下alpha为1
+        canvasGroup.alpha = 1;
+        hideCallBack = callBack;
+    }
+    #endregion
+    #region 其他方法
+    /// <summary>
+    /// 得到特定名称与特定类型的组件
+    /// </summary>
+    /// <param name="name">组件名</param>
+    /// <typeparam name="T">组件类型</typeparam>
+    /// <returns>特定种类的组件对象，没有返回空</returns>
+    public T GetControl<T>(string name) where T : UIBehaviour
+    {
+        if (controls.ContainsKey(name))
+        {
+            T control = controls[name] as T;
+            if (control == null)
+            {
+                Debug.LogError($"请求的{name}组件存在，但不是请求所需的{typeof(T)}类型");
+            }
+            return control;
+        }
+        else
+        {
+            Debug.LogError($"请求的{name}组件不存在");
+            return null;
+        }
+    }
+    #endregion
+
+}
