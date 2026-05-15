e9c66c9acf91b901887806746227bbc9149b9ba6
AS17514 <13313856146@163.com>
2026-05-15 15:31:31 +0800

    Update DiePanel.cs

--- TheLaw/Assets/Scripts/Gameplay/UI/DiePanel.cs
+++ TheLaw/Assets/Scripts/Gameplay/UI/DiePanel.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 
 public class DiePanel : PanelBase
 {
-    void Start()
-    {
-        EventCenter.Instance.EventTrigger(E_EventType.Audio_Play_BGM,
-            new object[] { E_BGM.GameOver, true });
-    }
-
     protected override void ButtonOnClick(string buttonName)
     {
         UIManager.Instance.RemovePanel<BattlePanel>();
