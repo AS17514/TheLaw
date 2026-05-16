3512994fdb5b6c2e80afcc8cb0a3d3f0aac4392e
AS17514 <13313856146@163.com>
2026-05-16 14:18:46 +0800

    受击音效更新

--- TheLaw/Assets/Scripts/Gameplay.Entities/Player.cs
+++ TheLaw/Assets/Scripts/Gameplay.Entities/Player.cs
@@ -67,6 +67,8 @@ public class Player : CharacterBase
         int i = 0;
         if (buffs[E_BuffType.Left] > 0)
             i = 1;
+        if (hp - damage > 0)
+            AudioManager.Instance.PlaySFX(E_SFX.PlayerBeHit, false);
         base.TakeDamage(damage+i+ProgressManager.Instance.TryGetEntity().GetDamageBonus());
         EventCenter.Instance.EventTrigger(E_EventType.UI_Update_PlayerHP);
         // 致死时 Die() 已由 CharacterBase.TakeDamage 调用，勿重复触发 UI_Update_PlayerDied
