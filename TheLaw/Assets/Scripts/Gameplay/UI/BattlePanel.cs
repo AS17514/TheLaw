f207a30c8d1c7787ead87613974ca0bfed40d8b7
AS17514 <13313856146@163.com>
2026-05-18 20:25:01 +0800

    文本

--- TheLaw/Assets/Scripts/Gameplay/UI/BattlePanel.cs
+++ TheLaw/Assets/Scripts/Gameplay/UI/BattlePanel.cs
@@ -920,7 +920,7 @@ public class BattlePanel : PanelBase
         {
             GetControl<Toggle>("Toggle_EntityPart0").interactable = false;
             GetControl<TextMeshProUGUI>("Text (TMP)_EntityPart0Name").text = "<color=grey>本体</color>";
-            GetControl<TextMeshProUGUI>("Text (TMP)_EntityName").text = "住";
+            GetControl<TextMeshProUGUI>("Text (TMP)_EntityName").text = "陈列室";
             Slider slider = GetControl<Slider>("Slider_EntityHP");
             slider.maxValue = 1;
             slider.value = 0;
