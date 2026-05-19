358bf61bd9bd2a5363abf5945b9c4b7ab27a85f9
AS17514 <13313856146@163.com>
2026-05-19 21:12:46 +0800

    Update E_TutorialType.cs

--- TheLaw/Assets/Scripts/Enums/E_TutorialType.cs
+++ TheLaw/Assets/Scripts/Enums/E_TutorialType.cs
@@ -7,8 +7,9 @@ public enum E_TutorialType
     None,
     MeetDodge,       // 第一次遇到闪避选项（怪物骰子池介绍）level1
     PartAppear,      // 第一次出现怪物部位level1
+    FirstCommunicate,// 第一次出现交流选项
     StateSwitch,     // 第一次状态切换
-    PlayerBuff,      //第一次玩家获得buff level2
+    PlayerBuff,      // 第一次玩家获得buff level2
     MeetManyDodge,   // 第一次遇到多个闪避选项  level3
     
 }
