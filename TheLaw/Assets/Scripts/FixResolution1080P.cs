3ec02bc1bab3ceae791f29f1ecd9ec97a51fe0f9
KAIbsb <lbky_1123@163.com>
2026-05-19 12:36:02 +0800

    问题修复
    修复了打包后pc端出直接全屏的问题

--- TheLaw/Assets/Scripts/FixResolution1080P.cs
+++ TheLaw/Assets/Scripts/FixResolution1080P.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
 
-public class FixResolution1080P : MonoBehaviour
+public class FixResolutionMobile : MonoBehaviour
 {
     void Awake()
     {
-        // 强制固定分辨率 1920*1080（标准16:9）
-        Screen.SetResolution(1920, 1080, true);
-        // 强制横屏
-        Screen.orientation = ScreenOrientation.LandscapeLeft;
-        // 禁止自动旋转
-        Screen.autorotateToLandscapeLeft = true;
-        Screen.autorotateToLandscapeRight = false;
-        Screen.autorotateToPortrait = false;
-        Screen.autorotateToPortraitUpsideDown = false;
+        if (Application.isMobilePlatform)
+        {
+            // 强制固定 1920x1080 16:9
+            Screen.SetResolution(1920, 1080, true);
+
+            // 强制横屏
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+            // 禁止旋转
+            Screen.autorotateToLandscapeLeft = true;
+            Screen.autorotateToLandscapeRight = false;
+            Screen.autorotateToPortrait = false;
+            Screen.autorotateToPortraitUpsideDown = false;
+        }
     }
 }
