e1aaf88b2189ff65642d46c669748481a7417ac0
KAIbsb <lbky_1123@163.com>
2026-05-20 22:12:03 +0800

    UI更新
    为结束界面添加星星闪动效果

--- TheLaw/Assets/Scripts/Gameplay/UI/EndingPanel.cs
+++ TheLaw/Assets/Scripts/Gameplay/UI/EndingPanel.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndingPanel : PanelBase
 {
     float _delay = 0.1f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        Image ciallo = GetControl<Image>("Image_Ciallo");
+        CanvasGroup cg = ciallo.GetComponent<CanvasGroup>();
+        cg.DOFade(0.15f, 0.7f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+    }
 
     void Update()
     {
