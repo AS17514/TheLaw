a4642138c7f8c806f2f1b3f734f1bc9a462d703a
AS17514 <13313856146@163.com>
2026-05-20 11:05:17 +0800

    新手引导更新

--- TheLaw/Assets/Scripts/Enums/E_TutorialType.cs
+++ TheLaw/Assets/Scripts/Enums/E_TutorialType.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public enum E_TutorialType
 {
     None,
+    Gamebigen,
     MeetDodge,       // 第一次遇到闪避选项（怪物骰子池介绍）level1
     PartAppear,      // 第一次出现怪物部位level1
     FirstCommunicate,// 第一次出现交流选项
-    StateSwitch,     // 第一次状态切换
-    PlayerBuff,      // 第一次玩家获得buff level2
-    MeetManyDodge,   // 第一次遇到多个闪避选项  level3
+    Changestate,     // 第一次状态切换
+    Level2begin,
     
 }
